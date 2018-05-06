55765adc5b8ecdea1ff9faaf7c0d2fe1d7229b04
Rui <eneblue@126.com>
2018-05-06 16:35:36 +0800

    [BUG] use "git config core.ignorecase false"

---
+++ server/models/Huaban.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+import './index';
+
+const HuabanSchema = new Schema({
+  pin_id: String,
+  img_hash: String,
+  width: Number,
+  height: Number,
+  username: String,
+  urlname: String,
+  raw_text: String,
+  load_time: Number,
+});
+
+export default mongoose.model('huaban_drawcrowd', HuabanSchema, 'huaban_drawcrowd');
