cd9b9c0a5b571b79bde6b38bd0cb273d0deb0e84
Rui <eneblue@126.com>
2018-05-29 21:49:14 +0800

    [Improve] Eslint support mocha

--- test/unit/user/login.test.js
+++ test/unit/user/login.test.js
@@ -1,33 +1,33 @@
-import supertest from 'supertest'
-import chai from 'chai'
-import 'babel-polyfill'
-import app from '../../../app'
+import supertest from 'supertest';
+// import chai from 'chai';
+import 'babel-polyfill';
+import app from '../../../app';
 
-const expect = chai.expect
+// const expect = chai.expect;
 
-const request = supertest(app.listen())
+const request = supertest(app.listen());
 
-describe ('Start Testing Login function POST Request', () => {
+describe('Start Testing Login function POST Request', () => {
   const userData = {
     username: 'test@test.com',
     password: 111111,
-  }
+  };
 
   it('Fail To Register User', (done) => {
     request
-      .post(`/auth/register`)
+      .post('/auth/register')
       .expect(200)
       .expect({
         data: {
-          error: ""
+          error: '',
         },
       })
-      .end(done)
-  })
+      .end(done);
+  });
 
   it('success create user', (done) => {
     request
-      .post(`/auth/test`)
+      .post('/auth/test')
       .send(userData)
       .expect(200)
       .expect({
@@ -35,6 +35,6 @@ describe ('Start Testing Login function POST Request', () => {
           email: userData.email,
         },
       })
-      .end(done)
-  })
+      .end(done);
+  });
 });
